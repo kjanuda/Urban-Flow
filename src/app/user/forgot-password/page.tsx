f7902e02f40af38ad9bdcda9e173c719b7d082f7
Januda J Kodithuwakku <113584734+kjanuda@users.noreply.github.com>
2025-11-16 19:18:53 +0530

    Update page.tsx

--- src/app/user/forgot-password/page.tsx
+++ src/app/user/forgot-password/page.tsx
@@ -21,7 +21,7 @@ export default function ForgotPasswordPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/forgot-password", {
+      const res = await fetch("https://cityreg.onrender.com/api/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
